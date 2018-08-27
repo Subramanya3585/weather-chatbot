e2145115c82adb6884f7136d9fd7db6e9fed6723
Subramanya3585 <subramanya.raj19@gmail.com>
2018-08-27 20:14:59 +0530

    Update run_app.py

--- run_app.py
+++ run_app.py
@@ -7,9 +7,9 @@ from rasa_slack_connector import SlackInput
 nlu_interpreter = RasaNLUInterpreter('./models/nlu/default/weathernlu')
 agent = Agent.load('./models/dialogue', interpreter = nlu_interpreter)
 
-input_channel = SlackInput('xoxp-394902393764-396202078743-395141031122-8999cebe2c5905be0e0753786c89348e', 
-		            'xoxb-394902393764-395141032434-oZgCi1tiDnzJjeZ8NJYGqZdF', 
-			    'WiNi5l2Yq8tiGgGNHvx33YZr', 
+input_channel = SlackInput('', 
+		            '', 
+			    '', 
 			    True)
 
 agent.handle_channel(HttpInputChannel(5005, '/', input_channel))
